3b4385abc21fd184a27fbb9ec905a54bf39f697c
Nguyễn Công Hiền <Hienqt0205@gmail.com>
2022-09-24 10:08:15 +0700

    Update6
    add footStep sound effect

---
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private AudioClip footStep;
+    [SerializeField] private AudioClip runSound;
+    private AudioSource audioSource;
+    private Boolean isRunning;
+    private Vector3 PlayerMovementInput;
+    private Vector2 PlayerMouseInput;
+    private float xRot;
+    [SerializeField] private Transform PlayerCamera;
+    [SerializeField] private Rigidbody PlayerBody;
+    [SerializeField] private float Speed;
+    [SerializeField] private float Sensitivity;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {
+            PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            MovePlayerCamera();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            audioSource.Play();
+            MovePlayer();
+            footStepControl();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void MovePlayer()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            Speed = 10;
+            isRunning = true;
+        }
+        if(Input.GetKey(KeyCode.LeftShift) == false)
+        {
+            Speed = 5;
+            isRunning = false;
+        }
+        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput) * Speed;
+        PlayerBody.velocity = new Vector3(MoveVector.x, PlayerBody.velocity.y, MoveVector.z);
+    }
+
+    void MovePlayerCamera()
+    {
+        xRot -= PlayerMouseInput.y * Sensitivity;
+        transform.Rotate(0f, PlayerMouseInput.x, 0f);
+        PlayerCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+    }
+
+    void footStepControl()
+    {
+        if (audioSource.isPlaying == false)
+        {
+            if (isRunning)
+            {
+                audioSource.PlayOneShot(runSound, 0.5f);
+            }
+            else
+            {
+                audioSource.PlayOneShot(footStep, 0.3f);
+            }
+        }
+    }
+}
