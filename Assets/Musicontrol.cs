696301726bc2616efed00545003cc58454044f2e
Lê Khắc Duy <leduy8677@gmail.com>
2022-10-04 23:34:14 +0700

    update 19
    sound bg

---
+++ Assets/Musicontrol.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicControlScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static MusicControlScript instance;
+
+    // Update is called once per frame
+    private void Awake(){
+        DontDestroyOnLoad(this.gameObject);
+        if(instance == null){
+            instance = this;
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+}
